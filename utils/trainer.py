f23951f207862ea35a92dce3169ea2ba00f35f02
Yanic Rothlingshofer <yrothlin-24@telecom-paris.fr>
2026-05-12 15:13:52 +0200

    update trainer

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,4 +1,32 @@
-
+import torch
+import torch.nn as nn
 
 class Trainer:
-    pass
+    def __init__(self):
+        pass
+
+
+    def _build_criterion(self):
+        pass
+    
+    def _build_optimizer(self):
+        pass
+
+    def _build_scheduler(self):
+        pass
+
+    def train_one_epoch(self):
+        pass
+
+    @torch.no_grad()
+    def validate_one_epoch(self):
+        pass
+
+
+    def train(self):
+        pass
+    
+    @torch.no_grad()
+    def evaluate(self):
+        pass
+
